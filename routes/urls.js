6535e25a26789b5bdc6f451f8affebd0ff3a6669
user883311 <tarik.chardon@gmail.com>
2018-03-25 00:28:46 +0800

    Replaced promise by async/await.

--- routes/urls.js
+++ routes/urls.js
@@ -18,7 +18,7 @@ mongoose.connect(db_url, function () {})
     .then(() => {
         console.log("Connected to MongoDB")
     })
-    .catch(err => console.error("Could not connect to MongoDB", err));
+    .catch(err => console.error("Could not connect to MongoDB.", err));
 
 
 // DATABASE MODEL
@@ -30,36 +30,51 @@ const urlSchema = new mongoose.Schema({
 const Url = mongoose.model("url", urlSchema); // class
 
 
-router.get("/new/(*)", (req, res) => {
-    console.log("router.get(/new called...");
-    /* URL Validation. 
-    -------------------
-    Credit on this regex to validate URLs goes to Daveo
-    on https://stackoverflow.com/a/3809435 */
-    const urlReg = new RegExp(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
-    const schema_url = {
-        '0': Joi.string().regex(urlReg).required()
-    };
-    const result = Joi.validate(req.params, schema_url);
-    if (result.error) {
-        return res.status(400).send(result.error.details[0].message);
-    };
+router.get("/new/(*)", async (req, res) => {
+    try {
+        console.log(`router.get(/new/${req.params['0']} called...`);
+        /* URL Validation. 
+        -------------------
+        Credit on this regex to validate URLs goes to Daveo
+        on https://stackoverflow.com/a/3809435 */
+        const urlReg = new RegExp(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
+        const schema_url = {
+            '0': Joi.string().regex(urlReg).required()
+        };
+        const result = Joi.validate(req.params, schema_url);
+        if (result.error) {
+            console.log("Result.error in the Joi Schema");
+            return res.status(400).send(result.error.details[0].message);
+        };
 
-    // Add new URL to the database
-    countUrls().then((len) => {
-            len = len.toString();
-            createUrl(len, req.params['0']);
-            res.send({
-                "original_url": req.params['0'],
-                "short_url": `${hostname}:${port}/${len}`
-            });
-        })
-        .catch(err => console.error("Could not countUrls():", err));
+        // Add new URL to the database
+        let len = await countUrls();
+        len = len.toString();
+        const u = createUrl(len, req.params['0']);
+        res.send({
+            "original_url": req.params['0'],
+            "short_url": `${hostname}:${port}/${len}`
+        });
+    } catch (err) {
+        console.log("router.get(/new error:", err);
+        res.status(500).send("Internal server error. Adding the new URL failed.", err);
+    }
+
+    // countUrls().then((len) => {
+    //         len = len.toString();
+    //         createUrl(len, req.params['0']);
+    //         res.send({
+    //             "original_url": req.params['0'],
+    //             "short_url": `${hostname}:${port}/${len}`
+    //         });
+    //     })
+    //     .catch(err => console.error("Could not countUrls():", err));
 });
 
 router.get("/:id", (req, res) => {
     const id = req.params.id;
     if (!getSpecificUrl(id)) {
+        console.log("!getSpecificUrl");
         return res.status(404).send("This micro URL is not in our database.");
     }
 
@@ -117,6 +132,7 @@ async function getSpecificUrl(i) {
         return r;
     } catch (err) {
         console.log("getSpecificUrl() error:", err);
+        return 1;
     }
 };
 
