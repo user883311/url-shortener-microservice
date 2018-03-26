b6da89ef438228b60be9269e56cea82628271f30
user883311 <tarik.chardon@gmail.com>
2018-03-26 22:46:25 +0800

    Error handler.

--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,4 @@
 module.exports = function(err, req, res, next){
     // Log the exception
-    res.status(500).send("The operation failed");
+    res.status(500).send("The operation failed.", err);
 };
